7d50f32c8d8ebdc7c0d5ff240ba7c7ccc20f0a17
Stefano Gigli <stefanogigligigli@gmail.com>
2025-01-08 14:38:03 +0100

    shortened name to sd

--- src/bb.m
+++ src/bb.m
@@ -1,4 +1,7 @@
-function alpha = bb(f, g, x0, max_iter, tol)
+function [x_stat, g_stat] = bb(f, g, x0, max_iter, tol)
+  if nargin() < 3
+    error("not enough inputs supplied")
+  end
   if not(exist("max_iter"))
     max_iter = 1000
   end
@@ -10,14 +13,20 @@ function alpha = bb(f, g, x0, max_iter, tol)
   d = [-g(x(:,l))];
   alpha = [1];
   while norm(d(:,l)) > tol && l < max_iter
+    %% compute next step
     x = [x, x + alpha(l)*d(:,l)];
+    %% compute next gradient
     d = [d, -g(x(:,l+1))];
     s = x(:,l+1) - x(:,l);
     y = d(:,l+1) - d(:,l);
+    %% compute next alpha
     alpha = [alpha, (s' * s) / (s' * y)];
+    l+=1;
     if norm(d(:,l) - d(:,l-1)) < tol
       break
     end
-    l++;
   end
+  l
+  x_stat = x;
+  g_stat = d;
 end
--- src/poincare.m
+++ src/poincare.m
@@ -1,8 +1,21 @@
 function b = poincare(n)
-  %% initialize n random points on the unit square
-  %% the distance function
-  %% the baricenter function
+  %% random points on the unit square
   %% the evaluation function and its gradient
   %% perform the search
   %% plot results
 end
+
+function d = distance(p0, p1)
+  %% the distance function
+  d = arccosh(1 + 2*pow(norm(P1 - p2),2) / (pow(1 - norm(P1),2) + pow(1 - norm(P2),2)))
+end
+
+function b = baricenter(x, w, p)
+  %% the baricenter function
+  
+end
+
+function c = arccosh(x)
+  
+end
+
